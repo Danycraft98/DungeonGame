056c09b00e84ed4a35ad8a4b6858004a1d647c1c
Daniel Lee <lee.daniel.jhl@gmail.com>
2025-05-30 14:42:25 -0400

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GameProject.States;
+
+namespace GameProject;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private State _currentState;
+
+    private State _nextState;
+
+    public void ChangeState(State state) {
+        _nextState = state;
+    }
+
+    public Game1() {
+        _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferWidth = 1280;//GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        _graphics.PreferredBackBufferHeight = 900;//GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize() {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent() {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
+    }
+
+    protected override void Update(GameTime gameTime) {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+         if(_nextState != null)
+        {
+            _currentState = _nextState;
+            _nextState = null;
+        }
+        _currentState.Update(gameTime);
+        _currentState.PostUpdate(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime) {
+        GraphicsDevice.Clear(Color.Black);
+        _currentState.Draw(gameTime, _spriteBatch);
+        base.Draw(gameTime);
+    }
+}
